163b7d5164ed130b9ca6460fbc9f627eb77cd9f1
Renan Almeida <renanrun76@gmail.com>
2026-07-21 21:53:41 -0300

    aula 21/07/26

--- aula_05/aula_05.js
+++ aula_05/aula_05.js
@@ -1,18 +1,18 @@
-const API_URL = `https://6a557801e49d9eb2cc55c2f6.mockapi.io/produtos`;
+
+const API_URL = `https://6a5fe634b1933e9d25fcc879.mockapi.io/produtos`;
 
 async function buscarProdutos() {
     try {
         const response = await fetch(API_URL);
         const data = await response.json();
-        popularTabela(data);
+        popularProdutos(data);
     } catch (error) {
         console.error(error);
         alert("Não foi possível buscar os dados!")
     }
 }
 
-function popularTabela(data) {
-    const tbody = document.querySelector("tbody");
+function popularProdutos(data) {
     let html = "";
     for(const d of data) {
         html += `
@@ -21,18 +21,51 @@ function popularTabela(data) {
                 <td>${d.nome}</td>
                 <td>${d.preco}</td>
                 <td>${d.quantidade}</td>
-                
+                <td>${calcularTotal(d.preco, d.quantidade)}</td>
+                <td>
+                    <button class="btn btn-danger" onclick="apagarProduto(${d.id})">
+                        Remover
+                    </button>
+                    <button class="btn btn-primary">Atualizar</button>
+                </td>
             </tr>
         `;
     }
 
+    const tbody = document.querySelector('#table_produtos tbody');
     tbody.innerHTML = html;
 }
 
 function calcularTotal(preco, quantidade) {
-
+    resultadoTotal = Number(preco)*Number(quantidade)
+    return (resultadoTotal).toLocaleString("pt-BR", {style: "currency", currency: "BRL" });
 }
 
+
+async function apagarProduto(id) {
+    if(!confirm("Realmente deseja apagar este produto?")) {
+        return;
+    }
+
+    const url = `${API_URL}/${id}`;
+    try {
+        await fetch(url, {
+            method: "DELETE"
+        });
+    } catch (error) {
+        alert("Não foi possível apagar o produto!");
+    } finally {
+        buscarProdutos();
+    }
+}
+
+async function atualizarProduto() {
+    
+}
+
+async function criarProduto() {
+    
+}
 function init() {
     buscarProdutos();
 }
